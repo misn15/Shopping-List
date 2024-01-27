498ac0aa61d0dcc1896ea5154ceec162728b4749
Nicole Mis <misn15@bu.edu>
2024-01-26 22:57:05 -0500

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShoppingList
+//
+//  Created by Nicole Mis on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Nicole Mis on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
